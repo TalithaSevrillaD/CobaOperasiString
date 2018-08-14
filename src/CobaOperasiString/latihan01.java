e07bbe8ede8bfc2f639d8c29ea82af5b81c077e2
TALITHA <TALITHA@LAPTOP-RTUP5796>
2018-08-14 09:25:40 +0700

    Coba method length & isEmpty

--- src/CobaOperasiString/latihan01.java
+++ src/CobaOperasiString/latihan01.java
@@ -13,6 +13,18 @@ public class latihan01 {
         String identitas = "Talitha Sevrilla Duriga / XRPL2 / 35";
         System.out.println("Identitas : " + identitas);
 
+        //latihan01
+        String x = "Operasi";
+        System.out.println("isi variabel x : " + x);
+        System.out.println("\"" + x + "\" panjang karakter = " + x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variabel y : " + y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
+
+        
     }
     }
 
